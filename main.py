795d6f4d60da8daf09963713e84dcc09c4a4ade0
CosmoCat12 <tulyaganov.azamat@gmail.com>
2024-11-29 19:31:41 +0500

    init

---
+++ main.py
@@ -0,0 +1,28 @@
+from turtle import Screen, Turtle
+import pandas
+
+screen = Screen()
+screen.title("U.S States Game")
+screen.setup(725, 491)
+screen.bgpic("blank_states_img.gif")
+
+data = pandas.read_csv("50_states.csv")
+guessed_states = []
+all_states = data.state.to_list()
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state of the U.S? : ").title()
+    if answer_state == "Exit":
+        missing_states = [state for state in all_states if state not in guessed_states]
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state.capitalize()]
+        t.goto(state_data.x.item(), state_data.y.item())
+        t.write(answer_state)
+
+states_to_learn = []
